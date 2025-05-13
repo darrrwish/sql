0dbd9fc1a9cb6c675a1bc84827dea421db93d5d3
darrrwish <ahmeddarrwish@gmail.com>
2025-05-13 13:43:06 +0300

    up2

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,7 @@
 export const ThemeManager = {
   init: () => {
     const savedTheme = localStorage.getItem('theme') || 'dark-mode';
+    document.body.classList.remove('dark-mode', 'light-mode'); // إزالة الكلاسات القديمة
     document.body.classList.add(savedTheme);
     document.getElementById('themeToggle').innerHTML = 
       savedTheme === 'dark-mode' ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
